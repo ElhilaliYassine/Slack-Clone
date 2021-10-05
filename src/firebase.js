4fdf311961243e685e2e0c8cd80ed571cafc901e
Yassine Elhilali <elhilaliyacin@gmail.com>
2021-10-05 19:41:40 +0100

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,15 @@
-import firebase from 'firebase/compat'
+import firebase from "firebase/compat";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCn6uVVcCr8olbkeNTWT7JCEIU2YH5zuxA",
-    authDomain: "slack-clone-ee050.firebaseapp.com",
-    projectId: "slack-clone-ee050",
-    storageBucket: "slack-clone-ee050.appspot.com",
-    messagingSenderId: "308164849303",
-    appId: "1:308164849303:web:03087e34dec517f79183d2"
-}
+  //Config
+};
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const db = firebaseApp.firestore()
+const db = firebaseApp.firestore();
 
-const auth = firebase.auth()
+const auth = firebase.auth();
 
-const provider = new firebase.auth.GoogleAuthProvider()
+const provider = new firebase.auth.GoogleAuthProvider();
 
-export { db, auth, provider }
+export { db, auth, provider };
